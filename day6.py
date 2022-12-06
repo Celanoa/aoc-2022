309a51f8976d0b58e0c05b6b48155ff420a8a243
Celanoa <nathaniel.alteza@gmail.com>
2022-12-06 13:29:57 +0800

    Day6 - Post Cleanup

--- day6.py
+++ day6.py
@@ -1,26 +1,16 @@
 def day6(text_file,length=4):
     f = open(text_file, "r")
     inputs = f.readlines()
-    # total_score = 0
-    # total_score1 = 0
-    # j=0
-    # k=0
     for i, input in enumerate(inputs):
         input = input.strip()
         for j in range(len(input)-length):
             buffer = input[j:j+length]
             for k,char in enumerate(buffer):
                 counts = buffer.count(char)
-                print(buffer)
                 if counts>1:
                     break
                 if k==length-1:
                     return(j+length,buffer)
-                # print(j,buffer)
-    # return(input)
-
-
-
 
 
 answers = day6('input6.txt')
---
+++ day7.py
@@ -0,0 +1,15 @@
+def day7(text_file):
+    f = open(text_file, "r")
+    inputs = f.readlines()
+    # total_score = 0
+    # total_score1 = 0
+    # j=0
+    # k=0
+    for i, input in enumerate(inputs):
+        input = input.strip()
+
+
+answers = day7('input7.txt')
+print(answers)
+answers = day7('input7.txt')
+print(answers)
